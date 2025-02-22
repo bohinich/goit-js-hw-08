65e8e36abad97b98324a7f7402001beb282fe7b4
Bohinich Serhii <bohinich1@gmail.com>
2025-02-22 23:30:36 +0200

    .

--- js/gallery.js
+++ js/gallery.js
@@ -63,3 +63,22 @@ const images = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = images
+    .map(({ preview, original, description }) =>
+        `<li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+        <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+    </li>
+    `)
+    .join("");
+
+galleryContainer.innerHTML = galleryMarkup;
